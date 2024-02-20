420a7d9d9e436b588c8bd1a9689ea1d5ecb1a4da
jeovane lima <145929373+JeovaneSilva@users.noreply.github.com>
2024-02-20 11:40:40 -0300

    update project

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,34 +1,12 @@
-// const URL = "https://api-galeria-designs-backend.vercel.app/"
-
-// async function initFirebase() {
-//   try {
-//     const response = await fetch('/');
-//     const data = await response.json();
-
-//     // Use os dados recebidos diretamente como o objeto Firebase config
-//     const app = firebase.initializeApp(data);
-//     const auth = app.auth();
-//     const storage = app.storage();
-//     const database = app.database();
-
-//     return { auth, storage, database };
-//   } catch (error) {
-//     console.error('Erro ao obter a configuração do Firebase:', error);
-//     throw error; // Rejeita a promessa com o erro
-//   }
-// }
-
-// export const { auth, storage, database } = await initFirebase(); // Importe as variáveis 
-
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC5E8XYNymJJbP9qvYBBobZyKvumqZkd2Y",
-  authDomain: "jean-designs.firebaseapp.com",
-  databaseURL: "https://jean-designs-default-rtdb.firebaseio.com",
-  projectId: "jean-designs",
-  storageBucket: "jean-designs.appspot.com",
-  messagingSenderId: "605079963145",
-  appId: "1:605079963145:web:66f230d23c4476d15e5ad8"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTHDOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
 };
 
 const app = firebase.initializeApp(firebaseConfig);
